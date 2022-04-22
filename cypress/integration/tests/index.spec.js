7ba8548a35100d4eb72ed49c10589d37af1696a8
unknown <robert_nokkvi@hotmail.com>
2022-04-22 22:17:48 +0000

    test

--- cypress/integration/tests/index.spec.js
+++ cypress/integration/tests/index.spec.js
@@ -8,21 +8,10 @@
 describe('index', () => {
   beforeEach(() => {
     cy.visit('https://vef2-2022-h2-mocha.vercel.app/');
-    cy.get('nav').contains('Matseðill').click();
   })
 
   it('visits matseðill', () => {
-
-    cy.visit('https://vef2-2022-h2-mocha.vercel.app/menu', {
-      timeout: 50000,
-      onBeforeLoad (contentWindow) {
-        // contentWindow is the remote page's window object
-        expect(typeof contentWindow === 'object').to.be.true
-      },
-      onLoad (contentWindow) {
-       // contentWindow is the remote page's window object
-       expect(typeof contentWindow === 'object').to.be.true
-      },
-    })
+    cy.contains('Matseðill').click();
+    cy.url().should('include', '/menu')
   })
 })
